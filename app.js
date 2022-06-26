d5cb0505234c3c3c7504f6fbadc2ae25f410a1ac
Carlos <75598221+unnativodigital@users.noreply.github.com>
2022-06-26 16:59:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+document.getElementById('form-task').addEventListener('submit',savetask,);
+
+
+function savetask(e) {
+
+    let title = document.getElementById ('title').value;
+    let description = document.getElementById ('description').value;
+
+    const task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem('tasks') === null) { 
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks)); 
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks)); 
+    }
+
+    getTasks();
+    document.getElementById('formTask').reset();
+    e.preventDefault();
+  }
+
+  
+    // testear si el evento funciona   alert("enviando formulario");
+    // preventDefault() para evitar el compoartamiento natural de la alerta.
+    // [{objeto dentro de arreglo}]
+
+
+// funcion borar tarea 
+function deleteTask(title) {
+    console.log(title)
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for(let i = 0; i < tasks.length; i++) {
+      if(tasks[i].title == title) {
+        tasks.splice(i, 1);
+      }
+    }
+    
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+  }
+  
+
+//funcion actualizar tarea
+
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+
+    tasksView.innerHTML = '';
+    
+    for(let i = 0; i < tasks.length; i++) {
+      let title = tasks[i].title;
+      let description = tasks[i].description;
+  
+      tasksView.innerHTML += `<div class="card mb-3">
+          <div class="card-body">
+            <p>${title} - ${description}
+            <a href="#" onclick="deleteTask('${title}')"  id="btn" class="btn btn-danger ml-5">Delete </a>
+            </p>
+          </div>
+        </div>`;
+    }
+  }
+  
+  getTasks();
